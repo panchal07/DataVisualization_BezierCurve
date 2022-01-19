2b056d4a6e056bfae95e0cabb17d8e5897c15dbc
panchal07 <85767921+panchal07@users.noreply.github.com>
2022-01-18 22:39:46 -0600

    Add files via upload

---
+++ bezier.js
@@ -0,0 +1,15 @@
+var cp_pos = [{cx:50, cy:250}, {cx:150, cy:50}, {cx:200, cy:50}, {cx:250, cy:250}];
+var cp_pos_text = [{x:50, y:250}, {x:150, y:50}, {x:200, y:50}, {x:250, y:250}];
+var data = [];
+var curve_been_created = false;
+
+
+function init() {
+  
+}
+
+function generate_bezier_curve(){
+  
+}
+
+init();
---
+++ part_1.js
@@ -0,0 +1,8 @@
+
+function generate_random_circles(){
+  //TODO
+}
+
+function remove_circles(){
+  //TODO
+}
